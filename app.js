f2be9773ae1ac00626f8faa1b6c018c74caa843d
Lokesh Kumar Mahawar <lokeshkumarmahawar79@gmail.com>
2021-01-22 15:06:24 +0530

    Express Sessions part 2

--- app.js
+++ app.js
@@ -42,47 +42,27 @@ app.use(session({
   store: new FileStore()
 }))
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 function auth(req,res,next) {
     console.log(req.session);
 
     if(!req.session.user) {
-      var authHeader = req.headers.authorization;
 
-      if( !authHeader ){
         var err = new Error('You are not Authonticated!');
   
-        res.setHeader('WWW-Authenticate', 'Basic');
         err.status = 401;
         return next(err)
-      }
   
-      var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  
-      var username = auth[0];
-      var password = auth[1];
-  
-      if ( username === 'admin' && password === 'password'){
-        // res.cookie('user', 'admin', { signed: true })
-        req.session.user = 'admin';
-        next();
-      }
-      else {
-        var err = new Error('Either Username or password inCorrect! or Both inCorret!');
-  
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        return next(err);
-  
-      }
     }
     else{
-      if(req.session.user === "admin"){
+      if(req.session.user === "authenticated"){
         next();
       }
       else{
         var err = new Error('You are not Authenticated!')
-        res.status = 401;
+        res.status = 403;
         return next(err);
       }
     }
@@ -94,8 +74,7 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
