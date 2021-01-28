3cae87b34f569f9e407045cfc79222e2ddbe06f6
Lokesh Kumar Mahawar <lokeshkumarmahawar79@gmail.com>
2021-01-28 14:45:31 +0530

    User Admin setUp

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -21,7 +21,7 @@ dishRouter.route('/' )
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post( authenticate.verifyUser, (req,res,next)=> {
+.post( authenticate.verifyUser, authenticate.adminUser, (req,res,next)=> {
     Dishes.create(req.body)
     .then((dish) => {
         console.log('Dish Created', dish);
@@ -31,11 +31,11 @@ dishRouter.route('/' )
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.put(authenticate.verifyUser, (req,res,next) => {
+.put(authenticate.verifyUser, authenticate.adminUser, (req,res,next) => {
     res.end('PUT operation not supported on /dishes');
     res.statusCode = 403;
 })
-.delete(authenticate.verifyUser, (req,res,next) => {
+.delete(authenticate.verifyUser, authenticate.adminUser, (req,res,next) => {
     Dishes.remove({})
     .then((resp) => {
         res.statusCode = 200;
@@ -56,13 +56,13 @@ dishRouter.route('/:dishId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post(authenticate.verifyUser, (req,res,next)=> {
+.post(authenticate.verifyUser, authenticate.adminUser, (req,res,next)=> {
     res.statusCode = 403;
     res.end('POST operation not supported on /dishes/'+
         req.params.dishId);
 
 })
-.put(authenticate.verifyUser, (req,res,next) => {
+.put(authenticate.verifyUser, authenticate.adminUser, (req,res,next) => {
     Dishes.findByIdAndUpdate(req.params.dishId, {
         $set: req.body
     },{ new: true })
@@ -73,7 +73,7 @@ dishRouter.route('/:dishId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.delete(authenticate.verifyUser, (req,res,next) => {
+.delete(authenticate.verifyUser, authenticate.adminUser, (req,res,next) => {
     Dishes.findByIdAndRemove(req.params.dishId)
     .then((resp) => {
         res.statusCode = 200;
@@ -87,7 +87,7 @@ dishRouter.route('/:dishId')
 dishRouter.route('/:dishId/comments' )
 .get((req,res,next) => {
     Dishes.findById(req.params.dishId)
-    .populate('comments.author')
+    .populate('comment.author')
     .then((dish) => {
         if(dish != null) {
             res.statusCode = 200;
@@ -131,7 +131,7 @@ dishRouter.route('/:dishId/comments' )
         +req.params.dishId + '/comments');
     res.statusCode = 403;
 })
-.delete(authenticate.verifyUser, (req,res,next) => {
+.delete(authenticate.verifyUser, authenticate.adminUser, (req,res,next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
         if(dish != null) {
